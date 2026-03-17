9e929fbfd568351d39b2b50c254501fd2cb6a2e0
Ankit Sharma <ankitconnect10@gmail.com>
2026-03-17 17:10:08 +0530

    clean repo without build files

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  output: "export",
+
+  // ✅ correct
+  assetPrefix: "",
+  trailingSlash: true,
 };
 
 module.exports = withMDX(nextConfig);
